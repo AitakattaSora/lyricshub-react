4023e3ea99cbc6d30a0f83d4e20b571b6ca7f478
mansionair <momynov.sultan@gmail.com>
2020-08-15 14:12:30 +0600

    make header sticky

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import SearchBar from '../Content/SearchBar';
-import { Typography } from '@material-ui/core';
+import { Typography, AppBar } from '@material-ui/core';
 
 const Header = () => {
   return (
-    <div className='header'>
-      <SearchBar />
-      <Typography variant='h6' component='h1'>
-        <NavLink to='/'>LyricsHub</NavLink>
-      </Typography>
-    </div>
+    <AppBar position='sticky'>
+      <div className='header'>
+        <SearchBar />
+        <Typography variant='h6' component='h1'>
+          <NavLink to='/'>LyricsHub</NavLink>
+        </Typography>
+      </div>
+    </AppBar>
   );
 };
 
